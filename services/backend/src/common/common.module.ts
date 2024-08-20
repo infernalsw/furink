db4effc3cf4b1be17860ae02a7eafa48bdee5734
kaylendog <actuallyori@gmail.com>
2024-08-20 18:07:33 +0100

    feat: add prisma client

--- services/backend/src/common/common.module.ts
+++ services/backend/src/common/common.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from "@nestjs/common";
 
 import { ConfigService } from "./providers/config.service";
+import { PrismaService } from "./providers/prisma.service";
 
 @Module({
-	providers: [ConfigService],
-	exports: [ConfigService],
+	providers: [ConfigService, PrismaService],
+	exports: [ConfigService, PrismaService],
 })
 @Global()
 export class CommonModule {}
